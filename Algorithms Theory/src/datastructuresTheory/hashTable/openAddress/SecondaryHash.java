ab528d137254a55d9e9b62c066e72514b40f0e79
hhoangphuoc <hohoangphuoc2002@gmail.com>
2022-02-21 20:02:45 +0100

    hash table + probing implementation

---
+++ Algorithms Theory/src/datastructuresTheory/hashTable/openAddress/SecondaryHash.java
@@ -0,0 +1,14 @@
+package datastructuresTheory.hashTable.openAddress;
+
+/**
+ * Simple interface file for double hashing
+ *
+ * @author William Fiset, william.alexandre.fiset@gmail.com
+ */
+
+
+public interface SecondaryHash {
+
+  // A method for a second hash function
+  public int hashCode2();
+}
